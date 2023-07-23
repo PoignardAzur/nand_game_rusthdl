08979e5f0a590d11fd9a530fe859e94e3debd0c8
Olivier FAURE <couteaubleu@gmail.com>
2023-07-23 12:26:40 +0200

    Add invert level

---
+++ src/level_invert.rs
@@ -0,0 +1,52 @@
+use rust_hdl::prelude::*;
+
+#[derive(LogicBlock)]
+struct NotGate {
+    pub input_1: Signal<In, Bits<1>>,
+    pub output_signal: Signal<Out, Bits<1>>,
+}
+
+impl Default for NotGate {
+    fn default() -> Self {
+        Self {
+            input_1: Default::default(),
+            output_signal: Default::default(),
+        }
+    }
+}
+
+impl Logic for NotGate {
+    #[hdl_gen]
+    fn update(&mut self) {
+        self.output_signal.next = !self.input_1.val();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut sim = Simulation::new();
+        sim.add_testbench(move |mut endpoint: Sim<NotGate>| {
+            let mut x = endpoint.init()?;
+
+            x.input_1.next = true.into();
+            let mut x = endpoint.wait(10 * sim_time::ONE_MICROSECOND, x).unwrap();
+            sim_assert_eq!(endpoint, x.output_signal.val(), false, x);
+
+            x.input_1.next = false.into();
+            let x = endpoint.wait(10 * sim_time::ONE_MICROSECOND, x).unwrap();
+            sim_assert_eq!(endpoint, x.output_signal.val(), true, x);
+
+            endpoint.done(x)
+        });
+        sim.run_to_file(
+            Box::new(NotGate::default()),
+            5 * sim_time::ONE_SEC,
+            "notgate.vcd",
+        )
+        .unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod level_invert;
 mod level_nand;
 
+#[allow(unused)]
 const CLOCK_SPEED_HZ: u64 = 10_000;
 
 fn main() {
